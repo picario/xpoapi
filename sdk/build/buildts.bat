26e304e2342c5ea6eacc0b462737031aa6beca60
Jellis Hogenbijl <jellis@lucrasoft.nl>
2016-08-17 16:16:52 +0200

    Fixed build issue with compiling d.ts and js files

--- sdk/build/buildts.bat
+++ sdk/build/buildts.bat
@@ -1,5 +1,5 @@
 cd ..
 cd js\PicarioXPO.RenderAPI.V2
 
-"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.4\tsc" --removeComments --target ES5 --out ..\compiledJS\PicarioXPO.RenderAPI.V2.js .\PicarioXPO.RenderAPI.V2.ts
-"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.4\tsc" --declaration --out ..\compiledDTS\PicarioXPO.RenderAPI.V2.d.ts .\PicarioXPO.RenderAPI.V2.ts
+"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.4\tsc" --removeComments --target ES5 --out ..\compiled\PicarioXPO.RenderAPI.V2.js .\PicarioXPO.RenderAPI.V2.ts
+"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.4\tsc" --declaration --out ..\compiled\PicarioXPO.RenderAPI.V2.d.ts .\PicarioXPO.RenderAPI.V2.ts
