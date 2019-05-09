5c1028d93545f7a26a362e517707d70de5658d4f
casper folkers <c.folkers@lucrasoft.nl>
2019-05-09 12:13:16 +0200

    Added gloss to color url builder

--- sdk/c#/PicarioXPO.RenderAPI.V2/ColorKeys.cs
+++ sdk/c#/PicarioXPO.RenderAPI.V2/ColorKeys.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -7,6 +8,7 @@ namespace PicarioXPO.RenderAPI.V2
     internal static class ColorKeys
     {
         public const string Color = "&p.c=";
+        public const string ColorGloss = "&p.cg=";
 
         public static StringBuilder AppendColors(this StringBuilder stringBuilder, IEnumerable<XpoUrlObject> xpoUrlColors)
         {
@@ -15,25 +17,29 @@ namespace PicarioXPO.RenderAPI.V2
             if (!xpoUrlObjects.Any())
                 return stringBuilder;
 
-            var max = xpoUrlObjects.Select(x => x.Index).OrderByDescending(x => x).FirstOrDefault();
             var colorBuilder = new StringBuilder(Color);
+            CreateStringForKey(colorBuilder,xpoUrlObjects,Color,color => color.Color);
+            CreateStringForKey(colorBuilder,xpoUrlObjects,ColorGloss,color => color.Gloss);
+            stringBuilder.Append(colorBuilder);
+            return stringBuilder;
+        }
 
+        private static void CreateStringForKey(StringBuilder builder,IList<XpoUrlObject> objects ,string keyName, Func<XpoUrlColor, object> getProperty)
+        {
+            var max = objects.Select(x => x.Index).OrderByDescending(x => x).FirstOrDefault();
+            builder.Append(keyName);
             for (var i = 0; i <= max; i++)
             {
-                var colorObject = xpoUrlObjects.LastOrDefault(x => x.Index == i);
+                var colorObject = objects.LastOrDefault(x => x.Index == i);
                 if (colorObject == null)
-                    colorBuilder.Append(",");
+                    builder.Append(",");
                 else
                 {
-                    colorBuilder.Append(colorObject.Color.Color);
-                    colorBuilder.Append(",");
+                    builder.Append(getProperty(colorObject.Color));
+                    builder.Append(",");
                 }
             }
-
-            colorBuilder.Remove(colorBuilder.Length - 1, 1);
-            stringBuilder.Append(colorBuilder);
-
-            return stringBuilder;
+            builder.Remove(builder.Length - 1, 1);
         }
     }
 }
